dcc9235652b8abc1683535b40f096384cef7de97
MVBPinho <marcos.vb.pinho@live.com>
2021-04-01 20:38:45 -0300

    first commit

---
+++ employee/src/aplication/Program.java
@@ -0,0 +1,36 @@
+package aplication;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Employee;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Employee employee = new Employee();
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Name: ");
+		employee.name = sc.nextLine();
+		System.out.print("Gross salary: ");
+		employee.glossSalary = sc.nextDouble();
+		System.out.print("Tax: ");
+		employee.tax = sc.nextDouble();
+		
+		System.out.println();
+		System.out.println("Employee: " + employee);
+			
+		System.out.println();
+		System.out.print("Which percentage to increase salary? " );
+		double percentage = sc.nextDouble();
+		employee.increaseSalary(percentage);
+		
+		System.out.println();
+		System.out.println("Update data: " + employee);
+		
+		sc.close();
+	}
+}

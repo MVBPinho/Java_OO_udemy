20154115fff80c6236ad9cb1c9980eeff176c3a6
MVBPinho <marcos.vb.pinho@live.com>
2021-04-01 21:59:27 -0300

    Revert "anotacoes para funcoes String"
    This reverts commit a4bca475eb0a135bfbdbeee9a81ebc5185ad8730.

---
+++ student/src/aplication/Program.java
@@ -0,0 +1,29 @@
+package aplication;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Student;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		Student student = new Student();
+		
+		System.out.print("NAME: ");
+		student.name = sc.nextLine();
+		System.out.println("Note first quarter");
+		student.grade1 = sc.nextDouble();
+		System.out.println("Note second quarter");
+		student.grade2 = sc.nextDouble();
+		System.out.println("Note third quarter");
+		student.grade3 = sc.nextDouble();
+		
+		System.out.println("FINAL GRADE = " + student.finalGrade());
+		System.out.println(student.situation());
+
+		sc.close();
+	}
+}

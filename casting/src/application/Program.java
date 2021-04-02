54a5d2a18630cdf9a8639b54077279574ad2cb28
MVBPinho <marcos.vb.pinho@live.com>
2021-04-02 15:13:31 -0300

    projetos com foreach, if ternario e casting

---
+++ casting/src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+public class Program {
+
+	public static void main(String [] args) {
+		double a;
+		int b;
+
+		a = 5.0;
+		b = (int) a;
+		
+		System.out.println(a);
+		System.out.println(b);
+	}
+}

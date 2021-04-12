146e8fa2ee2e6b931e5d92a4ff12b5e66d234b04
MVBPinho <marcos.vb.pinho@live.com>
2021-04-12 15:34:04 -0300

    Mudanca da classe account para abstract

--- account/src/application/Program.java
+++ account/src/application/Program.java
@@ -1,5 +1,9 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
 import entities.Account;
 import entities.BusinessAccount;
 import entities.SavingsAccount;
@@ -8,7 +12,6 @@ public class Program {
 
 	public static void main(String [] args) {
 		
-		Account acc = new Account(1001, "Alex", 0.0);
 		BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
 		
 		// UPCASTING
@@ -33,10 +36,6 @@ public class Program {
 			System.out.println("Update!");
 		}
 		
-		Account acc6 = new Account(1001, "Alex", 1000.0);
-		acc6.withdraw(200.0);
-		System.out.println(acc6.getBalance());
-		
 		Account acc7 = new SavingsAccount(1002, "Maria", 1000.0, 0.01);
 		acc7.withdraw(200.0);
 		System.out.println(acc7.getBalance());
@@ -45,16 +44,28 @@ public class Program {
 		acc8.withdraw(200.0);
 		System.out.println(acc8.getBalance());
 		
-		//Polimorfismo
-		Account x = new Account(1020, "Alex", 1000.0);
-		Account y = new SavingsAccount(1023, "Maria", 1000.0, 0.01);
 		
-		x.withdraw(50.0);
-		y.withdraw(50.0);
+		Locale.setDefault(Locale.US);
 		
-		System.out.println(x.getBalance());
-		System.out.println(y.getBalance());
+		List<Account> list = new ArrayList<>();
 		
+		list.add(new SavingsAccount(1001, "Alex", 500.00, 0.01));
+		list.add(new BusinessAccount(1002, "Maria", 1000.0, 400.0));
+		list.add(new SavingsAccount(1004, "Bob", 300.0, 0.01));
+		list.add(new BusinessAccount(1005, "Anna", 500.0, 500.0));
 		
+		double sum = 0.0;
+		for (Account acc : list) {
+			sum += acc.getBalance();
+		}
+		
+		System.out.printf("Total balance: %.2f%n", sum);
+		
+		for (Account acc : list) {
+			acc.deposit(10.0);
+		}
+		for (Account acc : list) {
+			System.out.printf("Updated balance for account %d: %.2f%n", acc.getNumber(), acc.getBalance());
+		}
 	}
 }

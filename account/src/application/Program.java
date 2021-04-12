ab06630036be9efbbb2693736f273f1a9048dca5
MVBPinho <marcos.vb.pinho@live.com>
2021-04-12 13:27:55 -0300

    Polimorfismo, criacao do projeto, polymorphyism_employee e realizado polimorfismo no projeto account

--- account/src/application/Program.java
+++ account/src/application/Program.java
@@ -45,5 +45,16 @@ public class Program {
 		acc8.withdraw(200.0);
 		System.out.println(acc8.getBalance());
 		
+		//Polimorfismo
+		Account x = new Account(1020, "Alex", 1000.0);
+		Account y = new SavingsAccount(1023, "Maria", 1000.0, 0.01);
+		
+		x.withdraw(50.0);
+		y.withdraw(50.0);
+		
+		System.out.println(x.getBalance());
+		System.out.println(y.getBalance());
+		
+		
 	}
 }
